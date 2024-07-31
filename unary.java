77144d5387d2f51f5d6f0303463b0e486c524771
PINTU KUMAR SAH <126938670+pintukumar5628@users.noreply.github.com>
2024-07-31 23:53:28 +0530

    Unary operator

---
+++ unary.java
@@ -0,0 +1,8 @@
+public class unary {
+    public static void main(String[] args) {
+        int a = 10;  // Initialize a with 10
+        int b = ++a; // Pre-increment a and assign the result to b
+        System.out.println(a); // Print the value of a
+        System.out.println(b); // Print the value of b
+    }
+}

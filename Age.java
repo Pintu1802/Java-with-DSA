05e13952258702cfbc3878be8aef788921c37b9f
PINTU KUMAR SAH <126938670+pintukumar5628@users.noreply.github.com>
2024-08-03 23:05:27 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,12 @@
+public class Age {
+    public static void main(String[] args) {
+        int age = 22;  // Define the age variable with a value of 22
+
+        // Check if the person is 18 or older
+        if(age >= 18) {
+            System.out.println("adult : drive, vote");  // Print if the person is an adult
+        } else {
+            System.out.println("not adult");  // Print if the person is not an adult
+        }
+    }
+}
